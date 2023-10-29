86d0d715033c6191a05ea5e28188c9a6f096aad2
rosaainz <al2173040136@azc.uam.mx>
2023-10-28 19:09:40 -0600

    banda roba tamales arreglado

--- estructurada/bandaRobatamales.c
+++ estructurada/bandaRobatamales.c
@@ -5,15 +5,8 @@ int main(){
 
   scanf("%d%d", &t, &b);
 
-  int lider = t/2;
-
-  if( t%2 == 1){
-    lider += 1;
-  }
-
-  int banda = t/2;
-  if( banda %2== 1 ){
-    lider += 1;
-  }
-  printf("%d", lider);
+  int lider = t/2 + (t%2);
+  int banda = t - lider;
+  int resto = banda % (b-1);
+  printf("%d", lider+resto);
 }
