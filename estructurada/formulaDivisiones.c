86258123ad0e4a5c3791934ea7355612b640d753
rosaainz <al2173040136@azc.uam.mx>
2023-10-29 17:55:44 -0600

    ejercicio formulaDivisiones 100%

--- estructurada/formulaDivisiones.c
+++ estructurada/formulaDivisiones.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
   float x,y;
   scanf("%f%f", &x, &y);
 
+  float term1 = ((x*x*x) + (x*x))/((y*y)-y);
+  float term2 = (x/y)+5;
+  float num = term1 - term2;
+  float den = 2*x;
+  printf("%f", num/den);
   return 0;
 }
