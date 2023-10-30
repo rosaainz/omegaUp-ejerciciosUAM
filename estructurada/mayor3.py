872e87a15abe9aad202a73a5bbea455bed493a48
rosaainz <al2173040136@azc.uam.mx>
2023-10-29 19:52:30 -0600

    refactor mayor3 con python

---
+++ estructurada/mayor3.py
@@ -0,0 +1,14 @@
+def mayor(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+print(mayor(num1, num2, num3))
+
