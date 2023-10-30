43ac6af6c810e2ad656016f5c02c34fe35030d46
rosaainz <al2173040136@azc.uam.mx>
2023-10-29 19:54:16 -0600

    usar funcion max para mayor3 con python

--- estructurada/mayor3.py
+++ estructurada/mayor3.py
@@ -1,10 +1,5 @@
 def mayor(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num3:
-        return num2
-    else:
-        return num3
+    return max(num1, num2, num3)
 
 num1 = int(input())
 num2 = int(input())
