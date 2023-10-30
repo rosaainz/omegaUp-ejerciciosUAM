b6e8bd32109e36349034eeac5b8b346bdd82876b
rosaainz <al2173040136@azc.uam.mx>
2023-10-29 19:33:45 -0600

    ejercicio valen7 100%

--- estructurada/valen7.c
+++ estructurada/valen7.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 
 int main(){
-  int x,y;
-  scanf("%d%d", &x, &y);
-  retur 0;
+  int a,b;
+  int contador = 0;
+  scanf("%d%d", &a, &b);
+
+  contador += (a == 7);
+  contador += (b == 7);
+
+  printf("%d", contador);
+  return 0;
 }
