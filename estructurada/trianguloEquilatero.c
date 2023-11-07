2f782f617e77fe3bef096dc73553df8def28cc08
rosaainz <al2173040136@azc.uam.mx>
2023-11-06 23:14:10 -0600

    trianguloEquilatero 100% correcto

--- estructurada/trianguloEquilatero.c
+++ estructurada/trianguloEquilatero.c
@@ -4,7 +4,10 @@ int main(){
   int p1,p2,p3,p4;
 
   scanf("%d%d%d%d", &p1, &p2, &p3, &p4);
-  if(p1 == p2 || p1 == p3 || p1 == p4 ){
+  if( (p2 == p3 && p3 == p4 )||
+     (p1 == p3 && p3 == p4) ||
+    (p1 == p2 && p2 == p4) ||
+   (p1 == p2 && p2 == p3) ){
     printf("%d",1); //si se puede
   }else{
     printf("%d", 0); //no se puede
