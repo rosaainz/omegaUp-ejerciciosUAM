cc43a8170b82b3042efc609f9156d6ab6b11603d
rosaainz <al2173040136@azc.uam.mx>
2023-11-06 23:20:36 -0600

    trianguloEquilatero refactor

--- estructurada/trianguloEquilatero.c
+++ estructurada/trianguloEquilatero.c
@@ -4,12 +4,12 @@ int main(){
   int p1,p2,p3,p4;
 
   scanf("%d%d%d%d", &p1, &p2, &p3, &p4);
-  if( (p2 == p3 && p3 == p4 )||
-     (p1 == p3 && p3 == p4) ||
-    (p1 == p2 && p2 == p4) ||
-   (p1 == p2 && p2 == p3) ){
-    printf("%d",1); //si se puede
-  }else{
-    printf("%d", 0); //no se puede
-  }
+  int c1 = (p2 == p3 && p3 == p4);
+  int c2 = (p1 == p3 && p3 == p4);
+  int c3 = (p1 == p2 && p2 == p4);
+  int c4 = (p1 == p2 && p2 == p3);
+
+  printf("%d", c1 || c2 || c3 || c4);
+
+  return 0;
 }
