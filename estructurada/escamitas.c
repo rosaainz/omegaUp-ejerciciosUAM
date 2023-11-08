a316ff71a9cb43d5638dc4243fcaaed55cdfb8a6
rosaainz <al2173040136@azc.uam.mx>
2023-11-07 22:47:43 -0600

    escamitas 100%

--- estructurada/escamitas.c
+++ estructurada/escamitas.c
@@ -7,10 +7,10 @@ int main(){
 
   if(t < 21 ){
     printf("fria!\n");
-  }else if(t > 21 && t < 29){
-    printf("tibia\n");
   }else if(t > 29 ){
     printf("caliente!\n");
+  }else{
+    printf("tibia\n");
   }
 
   if( t < 15 || t > 34){
