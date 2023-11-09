92b7f1c398ee6b9dab2fd5b275ad24842dd8a5e9
rosaainz <al2173040136@azc.uam.mx>
2023-11-08 21:20:10 -0600

    dibujandoTriangulo sin centrar

---
+++ estructurada/dibujandoTringulo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+  int n;
+
+  scanf("%d", &n);
+
+  for(int a=1; a<=n; a+=2){
+    for(int i=1; i<=1; i++){
+      printf("@");
+    }
+    printf("\n");
+  }
+  return 0;
+}
