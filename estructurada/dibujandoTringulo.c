3a783064ef23c2f97f3ea7dd4e52b8f877aea62e
rosaainz <al2173040136@azc.uam.mx>
2023-11-08 21:26:31 -0600

    dibujandoTriangulo centrado 100% correcto

--- estructurada/dibujandoTringulo.c
+++ estructurada/dibujandoTringulo.c
@@ -6,6 +6,10 @@ int main(){
   scanf("%d", &n);
 
   for(int a=1; a<=n; a+=2){
+    int espIzq = (n-a)/2;
+    for(int i=1; i<= espIzq; i++){
+      printf(" ");
+    }
     for(int i=1; i<=a; i++){
       printf("@");
     }
