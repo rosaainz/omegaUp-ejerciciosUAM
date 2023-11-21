714efa045dafed36474bd90c8ad4a1f8e8a8aa17
rosaainz <al2173040136@azc.uam.mx>
2023-11-20 23:01:56 -0600

    potencia usando recursividad forma 1wq

--- ada/potencia_ejemplo.c
+++ ada/potencia_ejemplo.c
@@ -8,12 +8,23 @@ int potencia_iterativa(int a, int b){
   return res;
 }
 
+int potencia_recursiva1(int a, int b){
+  if(b == 0){
+    return 1;
+  }else if(b == 1){
+    return a;
+  }else{
+    return potencia_recursiva1(a, b-1) * a;
+  }
+}
+
 
 
 int main(){
   for(int i=0; i<= 20; ++i){
-    printf("%d\n",
-        potencia_iterativa(2, i));
+    printf("2^%d =  %d, %d\n",i,
+        potencia_iterativa(2, i),
+        potencia_recursiva1(2, i));
   }
 
   return 0;
