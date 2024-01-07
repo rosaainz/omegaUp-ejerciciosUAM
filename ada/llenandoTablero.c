525a1f8c7c98ab5417ade75a1e9fe0a182fbd793
rosaainz <al2173040136@azc.uam.mx>
2024-01-06 20:17:47 -0600

    ADA: Llenando un tablero con fichas - 90%

---
+++ ada/llenandoTablero.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int f(int n){
+  if(n==1){
+    return 1;
+  }else if (n == 2){
+    return 2;
+  }else{
+    return f(n-1) + f(n-2);
+  }
+}
+
+
+
+int main(){
+  int n;
+  scanf("%d", &n);
+
+  long long k = f(n), res=1;
+  for(int i = 0; i < n; ++i){
+    res *= k;
+  }
+  //int k = f(n);
+ // printf("%lld", (long long)pow(k, n));
+  printf("%lld\n", res);
+}
