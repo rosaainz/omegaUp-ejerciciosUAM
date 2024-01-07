586102afb57abefdb06ff069fc08fb347879e14d
rosaainz <al2173040136@azc.uam.mx>
2024-01-06 20:52:34 -0600

    ADA: Cadenas Binarias sin ceros consecutivos : inicio

---
+++ ada/cadenasBinariasSinCerosCons.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+long long f(int n, int k){
+  return 2;
+}
+
+
+int main(){
+  int n, k;
+  scanf("%d%d", &n, &k);
+
+  printf("%lld", f(n,k));
+}
--- ada/llenandoTablero.c
+++ ada/llenandoTablero.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int f(int n){
-  if(n==1){
+  if(n== 0 || n==1){
     return 1;
-  }else if (n == 2){
+  }else if(n == 2){
     return 2;
   }else{
     return f(n-1) + f(n-2);
