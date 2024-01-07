b9f29c3879bfba46c028ec65f8142f1767ae9cc7
rosaainz <al2173040136@azc.uam.mx>
2024-01-06 21:24:36 -0600

    ADA: Cadenas Binarias sin ceros consecutivos - 100%

--- ada/cadenasBinariasSinCerosCons.c
+++ ada/cadenasBinariasSinCerosCons.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
 long long f(int n, int k){
+  if(n == 0 || k==0){
+    return 1;
+  }else if(n==1){
+    return 2;
+  }else{
+    return f(n-1, k) + f(n-2, k-1);
+  }
   return 2;
 }
 
